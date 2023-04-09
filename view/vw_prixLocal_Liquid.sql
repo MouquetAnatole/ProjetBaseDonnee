7a5ea102b668efde2c3fe088864480901cfdb2d1
Anatole MOUQUET <anatole mouquet@yahoo.com>
2023-04-09 18:57:51 +0200

    wip suite facturation

--- view/vw_prixLocal_Liquid.sql
+++ view/vw_prixLocal_Liquid.sql
@@ -5,7 +5,14 @@ AS
 $$
     BEGIN
         IF NOT EXISTS (SELECT cout_deplacement AS cout FROM Liquide_geo WHERE id_geo__arrive=r id_liquide=l UNION SELECT prix AS cout FROM Fournisseur_liquide where l= id_liquide AND r IN (SELECT FRegionEnglobantes(Region_geo.id) FROM Region_geo where Fournisseur_geo.id_fournisseur=Fournisseur_liquide.id_fournisseur))
-
+        THEN
+            RAISE EXCEPTION 'ERREUR, impossible d acheminer le liquide';
+        ELSEIF EXISTS(SELECT prix AS cout FROM Fournisseur_liquide where l= id_liquide AND r IN (SELECT FRegionEnglobantes(Region_geo.id) FROM Region_geo where Fournisseur_geo.id_fournisseur=Fournisseur_liquide.id_fournisseur))
+            THEN 
+                RETURN SELECT prix AS cout FROM Fournisseur_liquide where l= id_liquide AND r IN (SELECT FRegionEnglobantes(Region_geo.id) FROM Region_geo where Fournisseur_geo.id_fournisseur=Fournisseur_liquide.id_fournisseur);
+        ELSE
+            RETURN (SELECT MIN(cout_deplacement) FROM Liquide_geo WHERE id_liquide=l AND id_geo__arrive=r AND id_geo__arrive IN(SELECT id_geo FROM Fournisseur_liquide AS fl JOIN Fournisseur_geo as fg ON fl.id_fournisseur=fg.id_fournisseur WHERE fl.id_liquide=l));
+            --Implementetion basique sans algo de parcour de graphe pour acheminer les liquides
         END IF;
     END;
 $$
@@ -14,3 +21,6 @@ LANGUAGE plpgsql ;
 
 CREATE OR REPLACE MATERIALIZED VIEW vw_prixLocal_Liquid AS
     SELECT Liquid.id, Region_geo.id , prixLocal(Liquid.id,Region_geo.id) from Liquid,Region_geo;
+
+
+REFRESH MATERIALIZED VIEW vw_prixLocal_Liquid;
