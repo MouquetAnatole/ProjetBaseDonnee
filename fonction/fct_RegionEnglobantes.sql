e88cc17aa2c27c0018c17a9a8a0241746699b9d7
Anatole MOUQUET <anatole mouquet@yahoo.com>
2023-04-10 17:26:32 +0200

    fct_RegionEnglobantes

--- fonction/fct_RegionEnglobantes.sql
+++ fonction/fct_RegionEnglobantes.sql
@@ -1,6 +1,23 @@
 --FRegionEnglobantes
 -- Fonction qui retourne la table des regions qui contiennent la region donner en entrée
 -- en entré : id_geo INTERGER 
-
+CREATE OR REPLACE FUNCTION FRegionEnglobantes(r integer)
+RETURNS table(
+    id_geo integer
+)
+AS
+$$
+    DECLARE
+        tmp integer;
+    BEGIN
+        tmp := r;
+        WHILE EXISTS(SELECT * FROM Geo_geo WHERE tmp=id_geo)
+        LOOP
+            tmp := (SELECT id_geo_macro FROM Geo_geo WHERE tmp=id_geo);
+            RETURN NEXT;
+        END LOOP;
+    END;
+$$
+LANGUAGE plpgsql ;
 
 
