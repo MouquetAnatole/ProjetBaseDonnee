892ca6f152c6b37278deedd6a5a02abc83472671
Anatole MOUQUET <anatole mouquet@yahoo.com>
2023-04-10 17:36:27 +0200

    highlight

--- Init.sql
+++ Init.sql
@@ -1,20 +1,20 @@
-\! echo '\033[0;32;40m   INIT :  \033[0,36;40m Pré processing \033[0m-- Dropping all'
+\! echo '\033[1;32;40m   INIT :  \033[0,36;40m Pré processing \033[0m-- Dropping all'
 \i fonction/dropAll.sql
 \i view/dropAll.sql
 \i TableSuppression.sql
 
-\! echo '\033[0;32;40m   INIT :  \033[0,36;40m Creation des tables\033[0m'
+\! echo '\033[1;32;40m   INIT :  \033[0,36;40m Creation des tables\033[0m'
 \i TableCreation.sql
 \i trigger.sql
 
-\! echo '\033[0;32;40m   INIT :  \033[0,36;40m Remplissages des tables\033[0m'
+\! echo '\033[1;32;40m   INIT :  \033[0,36;40m Remplissages des tables\033[0m'
 \i Insert.sql
 
-\! echo '\033[0;32;40m   INIT :  \033[0,36;40m Creation des Fonctions\033[0m'
+\! echo '\033[1;32;40m   INIT :  \033[0,36;40m Creation des Fonctions\033[0m'
 \i fonction/fct_RegionEnglobantes.sql
 \i fonction/fct_isRegionIn.sql
 \i fonction/fct_totalSub.sql
 
-\! echo '\033[0;32;40m   INIT :  \033[0,36;40m Creation des Vues\033[0m'
+\! echo '\033[1;32;40m   INIT :  \033[0,36;40m Creation des Vues\033[0m'
 \i view/vw_prixLocal_Liquide.sql
 \i view/vw_subLocal_Liquide.sql
