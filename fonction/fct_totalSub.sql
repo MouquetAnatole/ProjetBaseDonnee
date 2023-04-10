4d12ecc85d4f73527bbcf9e70cbc6b960d3e92a0
Anatole MOUQUET <anatole mouquet@yahoo.com>
2023-04-10 14:40:33 +0200

    correction fct_totalSub

--- fonction/fct_totalSub.sql
+++ fonction/fct_totalSub.sql
@@ -15,7 +15,7 @@ $$
             ELSE
                 IF (SELECT COUNT(*) FROM Client WHERE FisRegionIn(id_geo,(SELECT id_geo FROM Subvention WHERE id_ = id))) >= (SELECT val_lowerCap FROM Sub_valHab WHERE id_ = id)
                 THEN
-                    tmp := tmp + (SELECT val_const FROM Sub_valHab WHERE id_ = id)*MIN(SELECT val_upperCap FROM Sub_valHab WHERE id_ = id UNION SELECT COUNT(*) FROM Client WHERE FisRegionIn(id_geo,(SELECT id_geo FROM Subvention WHERE id_ = id)))
+                    tmp := tmp + (SELECT val_const FROM Sub_valHab WHERE id_ = id)*(SELECT MIN(val_upperCap) FROM Sub_valHab WHERE id_ = id UNION SELECT COUNT(*) FROM Client WHERE FisRegionIn(id_geo,(SELECT id_geo FROM Subvention WHERE id_ = id)));
                 END IF;
             END IF;
             id_ := id_ + 1;
@@ -45,7 +45,7 @@ $$
                 ELSE
                     IF (SELECT COUNT(*) FROM Client WHERE FisRegionIn(id_geo,(SELECT id_geo FROM Subvention WHERE id_ = id))) >= (SELECT val_lowerCap FROM Sub_valHab WHERE id_ = id)
                     THEN
-                        tmp := tmp + (SELECT val_const FROM Sub_valHab WHERE id_ = id)*MIN(SELECT val_upperCap FROM Sub_valHab WHERE id_ = id UNION SELECT COUNT(*) FROM Client WHERE FisRegionIn(id_geo,(SELECT id_geo FROM Subvention WHERE id_ = id)))
+                        tmp := tmp + (SELECT val_const FROM Sub_valHab WHERE id_ = id)*(SELECT MIN(val_upperCap) FROM Sub_valHab WHERE id_ = id UNION SELECT COUNT(*) FROM Client WHERE FisRegionIn(id_geo,(SELECT id_geo FROM Subvention WHERE id_ = id)))
                     END IF;
                 END IF;
             END IF;
@@ -76,7 +76,7 @@ $$
                 ELSE
                     IF (SELECT COUNT(*) FROM Client WHERE FisRegionIn(id_geo,(SELECT id_geo FROM Subvention WHERE id_ = id))) >= (SELECT val_lowerCap FROM Sub_valHab WHERE id_ = id)
                     THEN
-                        tmp := tmp + (SELECT val_const FROM Sub_valHab WHERE id_ = id)*MIN(SELECT val_upperCap FROM Sub_valHab WHERE id_ = id UNION SELECT COUNT(*) FROM Client WHERE FisRegionIn(id_geo,(SELECT id_geo FROM Subvention WHERE id_ = id)))
+                        tmp := tmp + (SELECT val_const FROM Sub_valHab WHERE id_ = id)*(SELECT MIN(val_upperCap) FROM Sub_valHab WHERE id_ = id UNION SELECT COUNT(*) FROM Client WHERE FisRegionIn(id_geo,(SELECT id_geo FROM Subvention WHERE id_ = id)))
                     END IF;
                 END IF;
             END IF;
